b1f9f5c4064df5fcd3d9f018daffd790612e0ed8
Samprati Kothari <58175707+samprati97@users.noreply.github.com>
2021-02-04 23:49:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def predict():
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
         else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+            return render_template('index.html',prediction_text="You Can Sell The Car at {} Lakhs.".format(output))
     else:
         return render_template('index.html')
 
